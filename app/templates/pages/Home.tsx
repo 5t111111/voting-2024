d1f6c0244417b0728bf1f11dbbc0edee7d7a8d56
Hirofumi Wakasugi <333180+5t111111@users.noreply.github.com>
2024-09-29 07:15:17 +0000

    all templates is now jsx

--- app/templates/pages/Home.tsx
+++ app/templates/pages/Home.tsx
@@ -1,9 +1,9 @@
 import { FC } from "hono/jsx";
-import { Layout } from "../layouts/Layout.ts";
 import { Vote } from "../../schemas/vote.ts";
 import { data } from "../../data.ts";
 import { Hero } from "../components/Hero.tsx";
 import { Card } from "../components/Card.tsx";
+import { Layout } from "../layouts/Layout.tsx";
 
 type Props = {
   voted: boolean;
